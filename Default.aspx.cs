4782bad851c995e39f35496a68f816d30872592e
anvh <anvh@elarion.com>
2012-04-08 22:59:08 +0700

    La la la

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -10,23 +10,7 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        using (SqlConnection scon = new SqlConnection
-       (System.Configuration.ConfigurationManager.ConnectionStrings["ApplicationServices"].ToString()))
-        {
-            scon.Open();
-            try
-            {
-                using (SqlCommand scom = new SqlCommand("Select description from News where id = -1", scon))
-                {
-                    SqlDataReader sreader = scom.ExecuteReader();
-                    sreader.Read();
-                    content.Text = sreader.GetValue(0).ToString();
-                }
-            }
-            catch (Exception ex)
-            {
-            }
-        }
+       
     }
 
 
